243e0a787e3f205da0070f7c38c6c81566800982
AlexeysShelyagin <42693753+AlexeysShelyagin@users.noreply.github.com>
2026-03-10 20:26:43 +0100

    Logo debug

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,7 @@ pygments_style = "pastie"
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ["_static"]
-html_css_files = ["css/extra.css"]
+html_css_files = ["css/extra.css?v=1"]
 templates_path = ["_templates"]
 
 html_logo = "_static/images/DevelDeck.png"
