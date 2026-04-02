fe241056d003ec5c87e36e7997b9583365567d05
AlexeysShelyagin <42693753+AlexeysShelyagin@users.noreply.github.com>
2026-04-02 16:17:30 +0300

    v0.2.2

--- src/config.h
+++ src/config.h
@@ -263,7 +263,7 @@ const char GAME_CONFIG_FILE_NAME[] PROGMEM = "game.ini";
 // Minor version number (x.X.x)
 #define DEVELDECK_API_VERSION_MINOR 2
 // Patch version number (x.x.X)
-#define DEVELDECK_API_VERSION_PATCH 1
+#define DEVELDECK_API_VERSION_PATCH 2
 
 const char DEVELDECK_API_VERSION[] PROGMEM = "0.1.1";
 
